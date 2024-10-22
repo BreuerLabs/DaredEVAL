d6c2bbeba7a483382d357739ec3d1dba1c893657
Shailen Smith <shailenkent27@gmail.com>
2024-10-22 11:58:12 -0400

    start abstract classifier

--- classifiers/abstractModel.py
+++ classifiers/abstractModel.py
@@ -1,1 +1,17 @@
 import torch
+
+class AbstractClassifier:
+    def __init__(self):
+        raise NotImplementedError ("this is an abstract class and should not be directly instantiated.")
+
+
+    def train(self, args):
+        pass
+
+
+    def forward(self, X):
+        pass
+
+    
+    def predict(self, X):
+        return 
