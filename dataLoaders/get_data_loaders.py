dc98db93a2412e04762db0f8107087cd74812768
RasmusTorp <s214597@dtu.dk>
2024-10-29 10:56:48 -0400

    More datasets

--- dataLoaders/get_data_loaders.py
+++ dataLoaders/get_data_loaders.py
@@ -1,16 +1,10 @@
-import torch
-import torchvision
-from torchvision import transforms, datasets
-
-import torch
 from torchvision import datasets, transforms
-
 import torch
-from torchvision import datasets, transforms
 from torch.utils.data import random_split
 
 def get_data_loaders(config):
     """Dynamically loads datasets based on the configuration."""
+    
     if config.dataset.dataset == "CIFAR10":
         transform = transforms.Compose([
             transforms.ToTensor(),
@@ -26,6 +20,24 @@ def get_data_loaders(config):
         ])
         full_train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
         test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+        
+    elif config.dataset.dataset == "FashionMNIST":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        full_train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+        test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+        
+    elif config.dataset.dataset == "CelebA":
+        transform = transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        full_train_dataset = datasets.CelebA(root='./data', split='train', download=True, transform=transform)
+        test_dataset = datasets.CelebA(root='./data', split='test', download=True, transform=transform)
+    
     else:
         raise ValueError(f"Unknown dataset: {config.dataset.dataset}")
 
