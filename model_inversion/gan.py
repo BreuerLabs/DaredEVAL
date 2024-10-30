9ce7c6917cbfa6e9eee4e21f358aecfc41665441
RasmusTorp <s214597@dtu.dk>
2024-10-30 15:13:34 -0400

    gan class first draft

--- model_inversion/gan.py
+++ model_inversion/gan.py
@@ -1,3 +1,54 @@
+import torch.nn as nn
+import torch
 
-
-
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.label_emb = nn.Embedding(10, 10)
+        
+        self.model = nn.Sequential(
+            nn.Linear(794, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x, labels):
+        x = x.view(x.size(0), 784)
+        c = self.label_emb(labels)
+        x = torch.cat([x, c], 1)
+        out = self.model(x)
+        return out.squeeze()
+    
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.label_emb = nn.Embedding(10, 10)
+        
+        self.model = nn.Sequential(
+            nn.Linear(110, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+    
+    def forward(self, z, labels):
+        z = z.view(z.size(0), 100)
+        c = self.label_emb(labels)
+        x = torch.cat([z, c], 1)
+        out = self.model(x)
+        print(x.shape)
+        return out.view(x.size(0), 28, 28)
