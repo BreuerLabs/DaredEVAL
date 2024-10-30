ed2cb37ebd14c8037cd9ed4445d9579c257b61cf
Shailen Smith <shailenkent27@gmail.com>
2024-10-30 15:11:10 -0400

    add wandb tracking check

--- train_classifier.py
+++ train_classifier.py
@@ -5,6 +5,7 @@ import os
 import torch
 from dataloaders.get_data_loaders import get_data_loaders
 from classifiers.cnn import CNN
+from classifiers.lasso_net import LassoNet
 
 @hydra.main(config_path="conf_classifier", config_name="config.yaml", version_base="1.3")
 def train_classifier(config):
@@ -36,7 +37,10 @@ def train_classifier(config):
     
     if config.model.name == "CNN":
         model = CNN(config)
-        
+    
+    if config.model.name == "LassoNet":
+        model = LassoNet(config)
+
     else:
         raise ValueError(f"Unknown model: {config.model.name}")
 
@@ -44,8 +48,10 @@ def train_classifier(config):
     model.train_model(train_loader, val_loader)
     
     test_loss, test_accuracy = model.evaluate(test_loader)
-    wandb.log({"test_loss": test_loss})
-    wandb.log({"test_accuracy": test_accuracy})
+    
+    if config.training.wandb.track:
+        wandb.log({"test_loss": test_loss})
+        wandb.log({"test_accuracy": test_accuracy})
     
     
 if __name__ == "__main__":
