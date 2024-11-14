7f46479bae3a1f36e4e61ddfdb4c1d369782dac4
RasmusTorp <s214597@dtu.dk>
2024-11-14 12:45:18 -0500

    moved into subfolder

--- model_inversion/plug_and_play/own_approach/attack.py
+++ model_inversion/plug_and_play/own_approach/attack.py
@@ -1,8 +1,8 @@
 import torch
 
-from model_inversion.plug_and_play.gan import GAN
-from model_inversion.plug_and_play.stylegan import load_discrimator, load_generator
-from model_inversion.plug_and_play.utils import create_initial_vectors, create_target_vector
+from model_inversion.plug_and_play.own_approach.gan import GAN
+from model_inversion.plug_and_play.own_approach.stylegan import load_discrimator, load_generator
+from model_inversion.plug_and_play.own_approach.utils import create_initial_vectors, create_target_vector
 
 def run(target_model, target_config, attack_config):
     device = attack_config.training.device
@@ -20,6 +20,15 @@ def run(target_model, target_config, attack_config):
         synthesis.num_ws = num_ws
         discriminator = torch.nn.DataParallel(D, device_ids=gpu_devices)
 
+    #! Create config parser from our config
+    
+    
+    #! Create BaseModel from our classifiers
+    
+    
+    
+    
+    
     
     # Create target vectors
     targets = create_target_vector(attack_config)
