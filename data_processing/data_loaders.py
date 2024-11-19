e0c37ccc7f65ec48ccc00fcfb4150a48e7d3c9f3
RasmusTorp <rasmustorp.ai@gmail.com>
2024-11-19 15:34:12 -0500

    restructure

---
+++ data_processing/data_loaders.py
@@ -0,0 +1,33 @@
+from torchvision import datasets, transforms
+import torch
+from torch.utils.data import random_split
+from data_processing.datasets import get_datasets
+
+def get_data_loaders(config):
+    """Dynamically loads datasets based on the configuration."""
+    train_dataset, val_dataset, test_dataset = get_datasets(config)
+
+    num_workers = config.training.dataloader_num_workers
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                            batch_size=config.model.hyper.batch_size,
+                                            shuffle=True,
+                                            num_workers=num_workers,
+                                            pin_memory=True,
+                                            drop_last=True,
+                                               )
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                            batch_size=config.model.hyper.batch_size,
+                                            shuffle=False,
+                                            num_workers=num_workers,
+                                            pin_memory=True,
+                                            drop_last=False,)
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                            batch_size=config.model.hyper.batch_size,
+                                            shuffle=False,
+                                            num_workers=num_workers,
+                                            pin_memory=True,
+                                            drop_last=False,)
+    
+    print(f"Data loaders for {config.dataset.dataset} loaded successfully")
+    
+    return train_loader, val_loader, test_loader
