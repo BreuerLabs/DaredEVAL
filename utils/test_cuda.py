d8c2954fc240f55d6766b10c5dc34bb1bea3dd7e
ShailenSmith <shailenkent27@gmail.com>
2024-11-21 12:37:57 -0500

    change pick gpu

--- utils/test_cuda.py
+++ utils/test_cuda.py
@@ -1,27 +1,22 @@
 import torch
+import subprocess as sp
+import os
 
 has_cuda = torch.cuda.is_available()
 print("Cuda available:", has_cuda)
 
 def pick_gpu(wait_one_gpu=False, gpu_idx=0):
-    command = "nvidia-smi --query-gpu=memory.free --format=csv"
-            memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
-            memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
-            if memory_free_values[gpu_idx] == 48676:
-                print(f"using GPU {gpu_idx}")
-                os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_idx)
-                return
 
-        while True:
-            command = "nvidia-smi --query-gpu=memory.free --format=csv"
-            memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
-            memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
-            for idx in range(len(memory_free_values)):
-                if memory_free_values[idx] == 48676:
-                    print(f"using GPU {idx}")
-                    os.environ["CUDA_VISIBLE_DEVICES"] = str(idx)
-                    return
-            print(f"GPUs not available, sleeping for 60 minutes")
-            time.sleep(1800)
-            print("30 minutes left")
-            time.sleep(1800)
+    command = "nvidia-smi --query-gpu=memory.free --format=csv"
+    memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+    for idx in range(len(memory_free_values)):
+        print(memory_free_values[idx])
+        if memory_free_values[idx] == 48670:
+            print(f"using GPU {idx}")
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(idx)
+            return
+    print(f"GPUs not available")
+
+pick_gpu()
+
