81548100fb5c9a5aa875fd84dd2001b072cbed02
RasmusTorp <rasmustorp.ai@gmail.com>
2024-11-21 12:10:50 -0500

    small changes and CelebA stuff

--- data_processing/datasets.py
+++ data_processing/datasets.py
@@ -1,8 +1,11 @@
 
 from torchvision import datasets, transforms
 from torch.utils.data import Subset
+from torch.utils.data import Subset
 import torch
 from torch.utils.data import random_split
+import numpy as np
+from collections import Counter
 import numpy as np
 from collections import Counter
 
@@ -40,7 +43,6 @@ def get_datasets(config):
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
         
-        import pdb; pdb.set_trace()
         full_train_dataset = datasets.CelebA(root='./data', split='all', target_type = config.dataset.target_type, download=False, transform=transform)
         
         full_train_dataset, test_dataset = get_subset(dataset = full_train_dataset, 
@@ -58,6 +60,75 @@ def get_datasets(config):
     train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
 
     return train_dataset, val_dataset, test_dataset
+
+
+def get_subset(dataset, N, seed):
+    
+    """
+    Processes the CelebA dataset to return training and test subsets containing only the top 1000 most common classes.
+
+    Parameters:
+    ----------
+    dataset : Dataset
+        The input CelebA dataset containing images and corresponding labels. It is assumed that the dataset
+        includes a 'label' or equivalent field indicating the class of each sample.
+
+    Returns:
+    -------
+    train_dataset : Dataset
+        A processed training subset of the CelebA dataset containing only the top 1000 most common classes.
+    
+    test_dataset : Dataset
+        A processed testing subset of the CelebA dataset containing only the top 1000 most common classes.
+
+    """
+    dataset.targets = dataset.identity
+
+    # Select the 1,000 most frequent celebrities from the dataset
+    targets = np.array([t.item() for t in dataset.identity])
+    ordered_dict = dict(
+        sorted(Counter(targets).items(),
+                key=lambda item: item[1],
+                reverse=True))
+    sorted_targets = list(ordered_dict.keys())[:N]
+
+    # Select the corresponding samples for train and test split
+    indices = np.where(np.isin(targets, sorted_targets))[0]
+    np.random.seed(seed)
+    np.random.shuffle(indices)
+    training_set_size = int(0.9 * len(indices))
+    train_idx = indices[:training_set_size]
+    test_idx = indices[training_set_size:]
+
+    # Assert that there are no overlapping datasets
+    assert len(set.intersection(set(train_idx), set(test_idx))) == 0
+
+    # # Set transformations
+    # self.transform = transform
+    # target_mapping = {
+    #     sorted_targets[i]: i
+    #     for i in range(len(sorted_targets))
+    # }
+
+    # self.target_transform = T.Lambda(lambda x: target_mapping[x])
+
+    # Split dataset
+    
+    train_dataset = Subset(dataset, train_idx)
+    # train_targets = np.array(targets)[train_idx]
+    # targets = [self.target_transform(t) for t in train_targets]
+    # self.name = 'CelebA1000_train'
+    
+    test_dataset = Subset(dataset, test_idx)
+    # test_targets = np.array(targets)[test_idx]
+    # self.targets = [self.target_transform(t) for t in test_targets]
+    # self.name = 'CelebA1000_test'
+
+    return train_dataset, test_dataset
+    
+
+
+
 
 
 def get_subset(dataset, N, seed):
