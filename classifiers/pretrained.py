cff72792dcf8dc4780afe3d074a9f80311cadce6
RasmusTorp <rasmustorp.ai@gmail.com>
2024-11-25 16:16:18 -0500

    pretrained cleanup and fix

--- classifiers/pretrained.py
+++ classifiers/pretrained.py
@@ -35,7 +35,6 @@ class PreTrainedClassifier(AbstractClassifier):
         #! Is this used? It is not defined to do so in the abstract classifier
         if self.config.dataset.n_classes != model.fc.out_features:
             # exchange last layer to match desired numbers of classes
-            model.fc = nn.Linear(model.fc.in_features, self.num_classes)
+            model.fc = nn.Linear(model.fc.in_features, self.config.dataset.n_classes)
 
         return model
-    
