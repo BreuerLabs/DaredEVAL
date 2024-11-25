4a1ba5d6fe6136c1228c25330ee4a2b279373b87
ShailenSmith <shailenkent27@gmail.com>
2024-11-25 14:17:49 -0500

    remove bitnet and start pretrained class

--- classifiers/get_model.py
+++ classifiers/get_model.py
@@ -2,6 +2,7 @@
 from classifiers.cnn import CNN
 from classifiers.mlp import MLP
 # from classifiers.bitnet import BitNet 
+from classifiers.pretrained import PreTrainedClassifier
 
 def get_model(config):
     if config.model.name == "CNN":
@@ -10,9 +11,11 @@ def get_model(config):
     elif config.model.name == "MLP":
         model = MLP(config)
         
-    elif config.model.name == "BitNet":
-        # model = BitNet(config)
-        pass
+    # elif config.model.name == "BitNet":
+    #     model = BitNet(config)
+
+    elif config.model.name == "PreTrainedClassifier":
+        model = PreTrainedClassifier(config)
 
     else:
         raise ValueError(f"Unknown model: {config.model.name}")
