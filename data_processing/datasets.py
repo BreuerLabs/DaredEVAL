ee4c0afb1d7fcb589703500f4caf297874e51c81
ShailenSmith <shailenkent27@gmail.com>
2025-01-01 16:21:24 -0500

    change train/val splitting, remove augmtn from val split, fix FS test split bug

--- data_processing/datasets.py
+++ data_processing/datasets.py
@@ -11,14 +11,17 @@ def get_datasets(config, train_transform, test_transform):
     
     if config.dataset.dataset == "CIFAR10":
         full_train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+        full_val_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=test_transform)
         test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
         
     elif config.dataset.dataset == "MNIST":   
         full_train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=train_transform)
+        full_val_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=test_transform)
         test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=test_transform)
         
     elif config.dataset.dataset == "FashionMNIST":
         full_train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=train_transform)
+        full_val_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=test_transform)
         test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=test_transform)
         
     elif config.dataset.dataset == "FaceScrub":
@@ -31,8 +34,13 @@ def get_datasets(config, train_transform, test_transform):
                                                  split_seed = config.training.seed,
                                                  transform = train_transform)
         
+        full_val_dataset = facescrub.FaceScrub(group = config.dataset.group,
+                                               train = True,
+                                               split_seed = config.training.seed,
+                                               transform = test_transform)
+        
         test_dataset = facescrub.FaceScrub(group = config.dataset.group,
-                                            train = True,
+                                            train = False,
                                             split_seed = config.training.seed,
                                             transform = test_transform)
         
@@ -50,7 +58,7 @@ def get_datasets(config, train_transform, test_transform):
         # Check if data is downloaded
         if not is_downloaded:
             try:
-                _ = datasets.CelebA(root='./data', split='all', target_type = config.dataset.target_type, download=True, transform=transform)
+                _ = datasets.CelebA(root='./data', split='all', target_type = config.dataset.target_type, download=True, transform=train_transform)
                 del _
             
             except Exception as e:
@@ -64,6 +72,13 @@ def get_datasets(config, train_transform, test_transform):
                                    N = config.dataset.n_classes
                                    )
         
+        full_val_dataset = CelebA_N_most_common(train=True,
+                                    split_seed=config.training.seed,
+                                    transform=test_transform,
+                                    root='./data/celeba',
+                                    N = config.dataset.n_classes
+                                    )
+        
         test_dataset = CelebA_N_most_common(train=False,
                                    split_seed=config.training.seed,
                                    transform=test_transform,
@@ -75,16 +90,19 @@ def get_datasets(config, train_transform, test_transform):
         raise ValueError(f"Unknown dataset: {config.dataset.dataset}")
 
     # Split the full training dataset into training and validation sets
+    generator1 = torch.Generator().manual_seed(config.training.seed)
     train_size = int((1 - config.training.validation_size) * len(full_train_dataset))
     val_size = len(full_train_dataset) - train_size
-    generator1 = torch.Generator().manual_seed(config.training.seed)
-    train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size], generator=generator1)
+
+    # code from https://discuss.pytorch.org/t/changing-transforms-after-creating-a-dataset/64929/8
+    indices = torch.randperm(len(full_train_dataset), generator=generator1)
+    train_dataset = torch.utils.data.Subset(full_train_dataset, indices[:-val_size])
+    val_dataset = torch.utils.data.Subset(full_val_dataset, indices[-val_size:])
+    # train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size], generator=generator1)
     
     train_dataset = AttackDataset(train_dataset)
 
-    val_dataset.transform = test_transform #! don't do train transforms on val dataset
-    val_dataset = AttackDataset(val_dataset)
-    
+    # val_dataset.dataset.transform = test_transform #! don't do train transforms on val dataset    
     #! Maybe also do: test_dataset = AttackDataset(test_dataset)
 
     return train_dataset, val_dataset, test_dataset
