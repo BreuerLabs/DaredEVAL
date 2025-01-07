264392d539adc68100342ed31ccd2628143e4571
ShailenSmith <shailenkent27@gmail.com>
2025-01-07 13:52:35 -0500

    split bash script into two

--- setup_files.bash
+++ setup_files.bash
@@ -17,27 +17,3 @@ rm -f stylegan2-ada-pytorch/.gitignore
 # Download pretrained file for StyleGAN2
 wget https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl -P stylegan2-ada-pytorch/
 wget https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/afhqdog.pkl -P stylegan2-ada-pytorch/
-
-# Add secret data
-mkdir -p utils/lambdalabs
-echo "secret_breuer-labs_c17dc1abad344b1eb25a1388a5d27073.MU4HQ3VE0DND231nvWRoHt3b6SVmC7kZ" > utils/lambdalabs/lambdalabs_api_key.txt
-
-# Step 2: Rest of setup
-echo "Pulling latest updates..."
-git pull
-
-# Create a screen session
-screen -S pnp -dm bash
-
-# Install necessary packages and set up the environment
-echo "Installing required packages..."
-sudo apt -y install python3.11-venv python3.11-dev
-python3.11 -m venv venv
-source ./venv/bin/activate
-
-echo "Installing Python dependencies..."
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m pip install torch==2.0.0+cu117 -f https://download.pytorch.org/whl/torch_stable.html
-
-echo "Setup complete!"
---
+++ setup_packages.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Step 2: Rest of setup
+echo "Pulling latest updates..."
+git pull
+
+# Create a screen session
+screen -S pnp -dm bash
+
+# Install necessary packages and set up the environment
+echo "Installing required packages..."
+sudo apt -y install python3.11-venv python3.11-dev
+python3.11 -m venv venv
+source ./venv/bin/activate
+
+echo "Installing Python dependencies..."
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m pip install torch==2.0.0+cu117 -f https://download.pytorch.org/whl/torch_stable.html
+
+echo "Setup complete!"
