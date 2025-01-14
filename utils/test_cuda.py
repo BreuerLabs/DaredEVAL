27e4c841f74561a42bf5888c1b9b3d794cdc99a3
ShailenSmith <shailenkent27@gmail.com>
2025-01-14 17:12:13 -0500

    comment out pick_gpu

--- utils/test_cuda.py
+++ utils/test_cuda.py
@@ -8,25 +8,25 @@ print("Cuda available:", has_cuda)
 
 def pick_gpu(wait=True):
 
-    command = "nvidia-smi --query-gpu=memory.free --format=csv"
+    # command = "nvidia-smi --query-gpu=memory.free --format=csv"
 
-    while True:
-        memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
-        memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
+    # while True:
+    #     memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    #     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
         
-        for idx in range(len(memory_free_values)):
-            print(memory_free_values[idx])
-            if memory_free_values[idx] >= 45000:
-                print(f"using GPU {idx}")
-                os.environ["CUDA_VISIBLE_DEVICES"] = str(idx)
-                return
+    #     for idx in range(len(memory_free_values)):
+    #         print(memory_free_values[idx])
+    #         if memory_free_values[idx] >= 45000:
+    #             print(f"using GPU {idx}")
+    #             os.environ["CUDA_VISIBLE_DEVICES"] = str(idx)
+    #             return
         
-        print(f"GPU not available")
-        if wait:
-            print("Trying again in 10 minutes...")
-            time.sleep(600)
-        else:
-            return
+    #     print(f"GPU not available")
+    #     if wait:
+    #         print("Trying again in 10 minutes...")
+    #         time.sleep(600)
+    #     else:
+    return
 
 # pick_gpu()
 
